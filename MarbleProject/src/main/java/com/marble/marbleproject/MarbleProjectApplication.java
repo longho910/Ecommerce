98862a6c7a122830d77064f40e789d6f1e928e5a
Long Ho <long.ho12021@gmail.com>
2023-06-28 22:36:35 +0700

    Initial Commit

---
+++ MarbleProject/src/main/java/com/marble/marbleproject/MarbleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.marble.marbleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarbleProjectApplication.class, args);
+    }
+
+}
