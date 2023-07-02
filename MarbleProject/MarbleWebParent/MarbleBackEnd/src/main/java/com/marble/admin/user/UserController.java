d72810889b269bb8a714644ca8b341ee899e3e21
Long Ho <long.ho12021@gmail.com>
2023-07-02 18:08:55 +0700

    Add Sorting User List Function for User Management

--- MarbleProject/MarbleWebParent/MarbleBackEnd/src/main/java/com/marble/admin/user/UserController.java
+++ MarbleProject/MarbleWebParent/MarbleBackEnd/src/main/java/com/marble/admin/user/UserController.java
@@ -6,6 +6,7 @@ import com.marble.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -26,12 +27,15 @@ public class UserController {
 
     @GetMapping("/users")
     public String listFirstPage(Model model) {
-        return listByPage(1, model);
+        return listByPage(1, model, "firstName", "asc");
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
-        Page<User> page = service.listByPage(pageNum);
+    public String listByPage(@PathVariable(name = "pageNum") int pageNum,
+                             Model model, @Param("sortField") String sortField, @Param("sortDir") String sortDir) {
+        System.out.println("Sort Field: " + sortField);
+        System.out.println("Sort Order: " + sortDir);
+        Page<User> page = service.listByPage(pageNum, sortField, sortDir);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USER_PER_PAGE + 1;
@@ -39,12 +43,17 @@ public class UserController {
         if (endCount > page.getTotalElements()) {
             endCount = page.getTotalElements();
         }
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
         model.addAttribute("currentPage", pageNum);
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("startCount", startCount);
         model.addAttribute("endCount", endCount);
         model.addAttribute("totalItems", page.getTotalElements());
         model.addAttribute("listUsers", listUsers);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
         return "users";
     }
 
