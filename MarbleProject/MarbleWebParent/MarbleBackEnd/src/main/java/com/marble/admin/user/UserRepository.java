2d94dc651cd54792a4918b157e88ab35de3b758c
Long Ho <long.ho12021@gmail.com>
2023-07-03 11:20:14 +0700

    Add Filter for User Management

--- MarbleProject/MarbleWebParent/MarbleBackEnd/src/main/java/com/marble/admin/user/UserRepository.java
+++ MarbleProject/MarbleWebParent/MarbleBackEnd/src/main/java/com/marble/admin/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.marble.admin.user;
 
 import com.marble.common.entity.User;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -14,6 +16,8 @@ public interface UserRepository extends CrudRepository<User, Integer>, PagingAnd
 
     public Long countById(Integer id);
 
+    @Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE %?1%")
+    public Page<User> findAll(String keyword, Pageable pageable);
     @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id=?1")
     @Modifying
     public void updateEnabledStatus(Integer id, boolean enabled);
