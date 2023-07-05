33445bc3d9dc7a46b2bc241f672241595a6c20cc
Long Ho <long.ho12021@gmail.com>
2023-07-05 10:57:58 +0700

    Add Responsive UI for small/medium screen

--- MarbleProject/MarbleCommon/src/main/java/com/marble/common/entity/User.java
+++ MarbleProject/MarbleCommon/src/main/java/com/marble/common/entity/User.java
@@ -139,4 +139,9 @@ public class User {
         }
         return "/users-photos/" + this.id + "/" + this.photos;
     }
+
+    @Transient
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
